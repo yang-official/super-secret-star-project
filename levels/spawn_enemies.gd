b5a9423fd7d0eb6705a031d365e9cf33997cba8e
Your Name <you@example.com>
2020-02-09 11:47:39 -0800

    added asteroid, spawns

---
+++ levels/spawn_enemies.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const asteroid = preload("res://entities/enemy/Asteroid.tscn")
+
+func _ready():
+	spawn()
+	pass
+
+func spawn():
+	while true:
+		randomize()
+		var enemy = asteroid.instance()
+		var pos = Vector2()
+		pos.x = rand_range(0+50, get_viewport().get_visible_rect().size.x - 50)
+		pos.y = 0 - 40
+		enemy.set_position(pos)
+		get_node("container").add_child(enemy)
+		yield(get_tree().create_timer(rand_range(0.5, 1.25)), "timeout")
+	pass
