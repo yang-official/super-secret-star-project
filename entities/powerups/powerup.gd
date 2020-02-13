e207d8f8ab510698677645455e16ce165ca7f7b3
Your Name <yang.official@gmail.com>
2020-02-13 10:55:05 -0800

    initialized powerups

---
+++ entities/powerups/powerup.gd
@@ -0,0 +1,14 @@
+# powerups
+
+extends Area2D
+
+var velocity = Vector2(0, 200)
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	translate(velocity * delta)
+	if get_position().y - 20 >= get_viewport_rect().size.y:
+		queue_free()
+	pass
