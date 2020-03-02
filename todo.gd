a11619a259a1f4c5b0f273d3627425e694192e87
Your Name <yang.official@gmail.com>
2020-03-01 20:20:10 -0800

    initialized dialogue box

---
+++ todo.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+# Drops and pickups spawn on destruction
+# Cutscene system
+# Money and upgrade system
+# Custom non-random spawns
+# Main Menu
+# Level Select Hub
+# Sounds
