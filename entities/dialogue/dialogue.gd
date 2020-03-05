65abb141d1fd747aab2a667d6aae2396c9084540
Your Name <yang.official@gmail.com>
2020-03-04 20:25:51 -0800

    finished dialog demo

--- entities/dialogue/dialogue.gd
+++ entities/dialogue/dialogue.gd
@@ -1,17 +1,28 @@
 extends RichTextLabel
 
-var dialog = ["Hey! This is a placeholder!"]
+var dialog = ["Hey! This is a placeholder!",
+"Hey, this is part two!"]
 var page = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_bbcode(dialog[page])
 	set_visible_characters(0)
+	set_process_input(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _input(event):
+	if event is InputEventKey and event.get_scancode() == KEY_SPACE && event.is_pressed():
+		if get_visible_characters() > get_total_character_count():
+			if page < dialog.size() - 1:
+				page += 1
+				set_bbcode(dialog[page])
+				set_visible_characters(0)
+		else:
+			set_visible_characters(get_total_character_count())
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters()+1)
