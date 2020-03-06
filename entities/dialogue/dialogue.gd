898b08fd52be8d51f982bc99e11e764fdb0603ab
Your Name <yang.official@gmail.com>
2020-03-05 21:16:37 -0800

    start expanding dialogue system

--- entities/dialogue/dialogue.gd
+++ entities/dialogue/dialogue.gd
@@ -1,25 +1,32 @@
 extends RichTextLabel
 
-var dialog = ["Hey! This is a placeholder!",
-"Hey, this is part two!"]
+# var dialog = ["Hey! This is a placeholder!", "Hey, this is part two!"]
 var page = 0
 
-# Called when the node enters the scene tree for the first time.
+var dialogue_file_path = "res://assets/text/dialogue.json"
+var dialogue : Dictionary = load_dialogue(dialogue_file_path)
+
 func _ready():
-	set_bbcode(dialog[page])
+	var dialogue : Dictionary = load_dialogue(dialogue_file_path)
+	set_bbcode(dialogue[page]["text"])
 	set_visible_characters(0)
 	set_process_input(true)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func load_dialogue(file_path) -> Dictionary:
+	# Parses JSON file and returns it as a dictionary
+	var file = File.new()
+	assert file.file_exists(file_path)
+	file.open(file_path, file.READ)
+	var dialogue = parse_json(file.get_as_text())
+	assert dialogue.size() > 0
+	return dialogue
 
 func _input(event):
 	if event is InputEventKey and event.get_scancode() == KEY_SPACE && event.is_pressed():
 		if get_visible_characters() > get_total_character_count():
-			if page < dialog.size() - 1:
+			if page < dialogue.size() - 1:
 				page += 1
-				set_bbcode(dialog[page])
+				set_bbcode(dialogue[page]["text"])
 				set_visible_characters(0)
 		else:
 			set_visible_characters(get_total_character_count())
