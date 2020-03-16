8aca910798f026b62e1c5608ef8c2ab69bbd9940
Your Name <yang.official@gmail.com>
2020-03-16 16:32:04 -0700

    fixed json loading

--- entities/dialogue/dialogue.gd
+++ entities/dialogue/dialogue.gd
@@ -1,23 +1,21 @@
 extends RichTextLabel
 
-# var dialog = ["Hey! This is a placeholder!", "Hey, this is part two!"]
-var page = 0
-
+var page = "0"
 var dialogue_file_path = "res://assets/text/dialogue.json"
 
 func _ready():
 	var dialogue = load_dialogue(dialogue_file_path)
 	set_bbcode(dialogue[page]["text"])
-	set_visible_characters(0)
-	set_process_input(true)
+	#set_visible_characters(0)
+	#set_process_input(true)
 
 func load_dialogue(file_path) -> Dictionary:
 	# Parses JSON file and returns it as a dictionary
 	var file = File.new()
 	assert file.file_exists(file_path)
 	file.open(file_path, file.READ)
-	var dialogue = parse_json(file.get_as_text())
-	print(dialogue)
+	var file_text = file.get_as_text()
+	var dialogue = parse_json(file_text)
 	assert dialogue.size() > 0
 	return dialogue
 
@@ -25,8 +23,8 @@ func _input(event):
 	var dialogue = load_dialogue(dialogue_file_path)
 	if event is InputEventKey and event.get_scancode() == KEY_SPACE && event.is_pressed():
 		if get_visible_characters() > get_total_character_count():
-			if page < dialogue.size() - 1:
-				page += 1
+			if int(page) < dialogue.size() - 1:
+				page = str(int(page) + 1)
 				set_bbcode(dialogue[page]["text"])
 				set_visible_characters(0)
 		else:
