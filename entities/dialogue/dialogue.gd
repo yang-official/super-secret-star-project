27d72dd816a6a71a459ed5855058178737994831
Your Name <yang.official@gmail.com>
2020-03-30 14:28:43 -0700

    added name to dialogue

--- entities/dialogue/dialogue.gd
+++ entities/dialogue/dialogue.gd
@@ -1,12 +1,13 @@
-extends RichTextLabel
+extends Control
 
 var page = "0"
-var dialogue_file_path = "res://assets/text/dialogue.json"
+var dialogue_file_path = "res://assets/text/demo_dialogue.json"
 
 func _ready():
 	var dialogue = load_dialogue(dialogue_file_path)
-	set_bbcode(dialogue[page]["text"])
-	set_visible_characters(0)
+	$Text.set_bbcode(dialogue[page]["text"])
+	$Name.set_text(dialogue[page]["name"])
+	$Text.set_visible_characters(0)
 	set_process_input(true)
 
 func load_dialogue(file_path) -> Dictionary:
@@ -22,13 +23,14 @@ func load_dialogue(file_path) -> Dictionary:
 func _input(event):
 	var dialogue = load_dialogue(dialogue_file_path)
 	if event is InputEventKey and event.get_scancode() == KEY_SPACE && event.is_pressed():
-		if get_visible_characters() > get_total_character_count():
+		if $Text.get_visible_characters() > $Text.get_total_character_count():
 			if int(page) < dialogue.size() - 1:
 				page = str(int(page) + 1)
-				set_bbcode(dialogue[page]["text"])
-				set_visible_characters(0)
+				$Text.set_bbcode(dialogue[page]["text"])
+				$Name.set_text(dialogue[page]["name"])
+				$Text.set_visible_characters(0)
 		else:
-			set_visible_characters(get_total_character_count())
+			$Text.set_visible_characters($Text.get_total_character_count())
 
 func _on_Timer_timeout():
-	set_visible_characters(get_visible_characters()+1)
+	$Text.set_visible_characters($Text.get_visible_characters()+1)
